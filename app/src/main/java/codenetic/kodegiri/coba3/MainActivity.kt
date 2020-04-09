8b8c0dc0943499f45184ac54df94127f1b89ca10
Catacola <fufideira17@gmail.com>
2020-04-10 04:41:29 +0700

    PT.Kodegiri

---
+++ app/src/main/java/codenetic/kodegiri/coba3/MainActivity.kt
@@ -0,0 +1,12 @@
+package codenetic.kodegiri.coba3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
