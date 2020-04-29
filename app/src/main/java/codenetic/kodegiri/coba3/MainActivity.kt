6c3421d0111a77d7a16119ae87de7feee1002150
Catacola <fufideira17@gmail.com>
2020-04-30 02:14:03 +0700

    selesai login

--- app/src/main/java/codenetic/kodegiri/coba3/MainActivity.kt
+++ app/src/main/java/codenetic/kodegiri/coba3/MainActivity.kt
@@ -1,12 +1,101 @@
 package codenetic.kodegiri.coba3
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnlogin   : Button
+    private lateinit var email      : EditText
+    private lateinit var password   : EditText
+    private lateinit var btncreate  : TextView
+    private lateinit var reference: DatabaseReference
+    private var USERNAME_KEY = "username_key"
+    private var username_key = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
+        setContentView(R.layout.activity_main)
+
+        btnlogin    = findViewById(R.id.btn_login)
+        email       = findViewById(R.id.editTextEmail)
+        password    = findViewById(R.id.editTextPassword)
+        btncreate   = findViewById(R.id.createaccount)
+
+        btncreate.setOnClickListener({
+            intent = Intent(this, register::class.java)
+            startActivity(intent)
+        })
+        btnlogin.setOnClickListener({
+            // ubah state menjadi loading
+            btnlogin.isEnabled = false
+            btnlogin.setText("Loading ...")
+
+            val dUsername: String = email.text.toString()
+            val dPassword: String = password.text.toString()
+
+            if (dUsername.isEmpty()) {
+                btnlogin.isEnabled = true
+                btnlogin.setText("SIGN IN")
+                Toast.makeText(this@MainActivity, "username kosong", Toast.LENGTH_SHORT).show()
+            } else {
+                if (dPassword.isEmpty()) {
+                    btnlogin.isEnabled = true
+                    btnlogin.setText("SIGN IN")
+                    Toast.makeText(this@MainActivity, "Password kosong", Toast.LENGTH_SHORT).show()
+                } else {
+                    reference = FirebaseDatabase.getInstance()
+                        .reference
+                        .child("Users")
+                        .child(dUsername)
+
+                    reference.addListenerForSingleValueEvent(object: ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if(dataSnapshot.exists() && dUsername != ""){
+
+//                        Toast.makeText(this@SignInActivity, "Username Ditemukan", Toast.LENGTH_SHORT).show()
+                                //Ambil data dari firebase (password)
+                                val passwordFromFirebase: String = dataSnapshot.child("password").value.toString()
+
+                                //validasi password firebase
+                                if(dPassword == passwordFromFirebase){
+
+                                    //sharedPReference Username to local
+                                    //menyimpan kepada lokal storage/smartphone
+                                    val sharedPreferences: SharedPreferences = getSharedPreferences(USERNAME_KEY, Context.MODE_PRIVATE)
+                                    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                                    editor.putString(username_key, dUsername).apply()
+
+                                    //berpindah activity
+                                    val gotoHomeIntent = Intent(this@MainActivity, MainMenu::class.java)
+                                    startActivity(gotoHomeIntent)
+                                } else {
+                                    btnlogin.isEnabled = true
+                                    btnlogin.setText("SIGN IN")
+                                    Toast.makeText(this@MainActivity, "Password Salah", Toast.LENGTH_SHORT).show()
+                                }
+
+                            } else {
+                                btnlogin.isEnabled = true
+                                btnlogin.setText("SIGN IN")
+                                Toast.makeText(this@MainActivity, "Username atau Password Salah", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+
+                        }
+                    })
+
+                }
+            }
+        })
     }
 }
--- app/src/main/java/codenetic/kodegiri/coba3/register.kt
+++ app/src/main/java/codenetic/kodegiri/coba3/register.kt
@@ -1,12 +1,78 @@
 package codenetic.kodegiri.coba3
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.firebase.database.*
 
 class register : AppCompatActivity() {
+    private lateinit var btnBack: LinearLayout
+    private lateinit var btnContinue: Button
+    private lateinit var edtnama: EditText
+    private lateinit var edtnumbermobile: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtUsername: EditText
 
+    //Firebase RealtimeDatabase
+    private lateinit var reference: DatabaseReference // penyimpanan data secara lokal storage
+
+    private var USERNAME_KEY = "username_key"
+    private var username_key = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.register)
+
+        btnContinue = findViewById(R.id.signup)
+
+        //casting EditText
+        edtEmail = findViewById(R.id.editTextEmail)
+        edtnumbermobile = findViewById(R.id.editTextMobile)
+        edtPassword = findViewById(R.id.editTextPassword)
+        edtnama = findViewById(R.id.editTextName)
+        edtUsername = findViewById(R.id.editUserName)
+        //kembali ke halaman sign in
+
+        btnContinue.setOnClickListener(View.OnClickListener {
+
+            //menyimpan kepada lokal storage/smartphone
+            val sharedPreferences: SharedPreferences = getSharedPreferences(USERNAME_KEY, Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString(username_key, edtUsername.text.toString()).apply()
+
+            //menyimpan ke firebase database
+            reference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("Users")
+                .child(edtUsername.text.toString())
+
+            reference.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.ref.child("Username").setValue(edtUsername.text.toString())
+                    dataSnapshot.ref.child("Email").setValue(edtEmail.text.toString())
+                    dataSnapshot.ref.child("Name").setValue(edtnama.text.toString())
+                    dataSnapshot.ref.child("password").setValue(edtPassword.text.toString())
+                    dataSnapshot.ref.child("Number Mobile").setValue(edtnumbermobile.text.toString())
+                    dataSnapshot.ref.child("user_balance").setValue(25)
+                }
+
+                override fun onCancelled(dataSnapshot: DatabaseError) {
+
+                }
+            })
+
+            Toast.makeText(this, "Data telah di Simpan ke Database", Toast.LENGTH_SHORT).show()
+            // berpindah activity
+            val registerActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(registerActivityIntent)
+        })
     }
 }
